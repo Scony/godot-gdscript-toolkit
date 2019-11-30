16816cf452e3813ef40eb7dafdde1a10722ad875
Pawel Lampe <pawel.lampe@gmail.com>
2019-11-30 15:46:03 +0100

    some array values

---
+++ scripts/array_values.gd
@@ -0,0 +1,27 @@
+var x = []
+var xx = [1]
+var xxx = [1,]
+var xxxx = [1,2]
+var xxxxx = [1,2,3,]
+var xxxxxx = [
+]
+var xxxxxxx = [1
+			   ,
+			   2,
+]
+var xxxxxxxx = [
+	1,
+	2,
+	3,
+]
+# var xxxxxxxxx = [1,
+# 				 2,
+# 				 3,]
+# var y = [1,
+# 		 2,
+# 		 3,
+# 		 ]
+# var yy = [1,
+# 		  2,
+# 		  3
+#     ]
