804517d4b66939e875f679d1d6c16207592f5444
Pawel Lampe <pawel.lampe@gmail.com>
2019-12-01 23:58:22 +0100

    semicolon support added

---
+++ scripts/semicolon.gd
@@ -0,0 +1,10 @@
+const x = 1; const y = 2
+const xx = 1; const yy = 2;
+
+class X:
+	const x = 1; const y = 2
+	const xx = 1; const yy = 2;
+
+func foo():
+	var xxx = 1; var yyy = 2
+	var xxxx = 1; var yyyy = 2;
