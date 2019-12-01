0b56a3d48a27c2e09588bda2dc4562097aba1c9b
Pawel Lampe <pawel.lampe@gmail.com>
2019-12-01 15:28:52 +0100

    node path fix

--- scripts/expressions.gd
+++ scripts/expressions.gd
@@ -1,3 +1,4 @@
+extends Node
 const XXXX = preload('functions.gd')
 func bar():
 	pass
---
+++ scripts/node_paths.gd
@@ -0,0 +1,12 @@
+extends Node
+
+func foo():
+	$Child
+	$"path"
+	$'path'
+	$"""path"""
+	$Child/Sub
+	@"xx"
+	@'xx'
+	@"""xxxx"""
+	$Child/Sub.text = "xx"
