5215b0bf08443b127c0ca394174dca2cd4b6fc32
Pawel Lampe <pawel.lampe@gmail.com>
2019-12-01 14:24:59 +0100

    setget handled

---
+++ scripts/setget.gd
@@ -0,0 +1,14 @@
+var x setget sss, ggg
+var xx setget sss
+var xxx setget ,ggg
+var xxxx = 6 setget sss, ggg
+onready var y setget sss, ggg
+onready var yy = 7 setget sss, ggg
+export var z = 7 setget sss, ggg
+export (int) var zz setget sss, ggg
+
+func sss(v):
+	pass
+
+func ggg():
+	pass
