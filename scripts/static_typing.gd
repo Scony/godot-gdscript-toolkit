31e618eb9a9d9a280a233f956f9cf4f26638f09e
Pawel Lampe <pawel.lampe@gmail.com>
2019-12-01 15:08:55 +0100

    static typing

---
+++ scripts/static_typing.gd
@@ -0,0 +1,17 @@
+func foo(x, var y, z: int, var q: int, w = 7, var e: int = 7, r: int = 7, t := 8, var y := 9):
+	pass
+
+func bar() -> void:
+	pass
+
+var x: int
+var xx: int = 6
+var xxx := 7
+
+func baz():
+	var x: int
+	var xx: int = 6
+	var xxx := 7
+
+func expr():
+	5 as int
