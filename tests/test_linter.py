bbdc127a074353d40c83a6dff2b8141b521cf8f7
Pawel Lampe <pawel.lampe@gmail.com>
2019-12-06 18:59:52 +0100

    linter: function args num check

--- tests/test_linter.py
+++ tests/test_linter.py
@@ -1,3 +1,5 @@
+import pytest
+
 from gdtoolkit.linter import lint_code
 
 
@@ -5,11 +7,52 @@ def test_empty_code_linting():
     lint_code('')
 
 
-def test_function_name():
+def test_function_name():       # TODO: ok case
     code = """
 func SomeName():
     pass
 """
     outcome = lint_code(code)
+    assert len(outcome) == 1
     assert outcome[0].name == 'function-name'
+    assert outcome[0].line == 2
+
+
+@pytest.mark.parametrize('code', [
+"""
+func foo(a1:int,a2:int):
+    pass
+""",
+"""
+func foo(a1,a2):
+    pass
+""",
+"""
+func foo():
+    pass
+""",
+"""
+func foo() -> int:
+    pass
+""",
+])
+def test_function_args_ok(code):
+    outcome = lint_code(code)
+    assert len(outcome) == 0
+
+
+@pytest.mark.parametrize('code', [
+"""
+func foo(a1:int,a2:int,a3:int,a4:int,a5:int,a6:int,a7:int,a8:int,a9:int,a10:int,a11:int):
+    pass
+""",
+"""
+func foo(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11):
+    pass
+""",
+])
+def test_function_args_nok(code):
+    outcome = lint_code(code)
+    assert len(outcome) == 1
+    assert outcome[0].name == 'function-arguments-number'
     assert outcome[0].line == 2
