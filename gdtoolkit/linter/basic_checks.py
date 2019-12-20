7bf1f5be8c92c18bb3502e81438b16f11ad577ce
Pawel Lampe <pawel.lampe@gmail.com>
2019-12-20 17:36:25 +0100

    'expression-not-assigned' check

--- gdtoolkit/linter/basic_checks.py
+++ gdtoolkit/linter/basic_checks.py
@@ -12,6 +12,10 @@ def lint(parse_tree, config):
             'unnecessary-pass',
             _unnecessary_pass_check,
         ),
+        (
+            'expression-not-assigned',
+            _expression_not_assigned_check,
+        ),
     ]
     problem_clusters = map(
         lambda x: x[1](parse_tree) if x[0] not in disable else [], checks_to_run_w_tree
@@ -34,6 +38,25 @@ def _unnecessary_pass_check(parse_tree): # performance... use AST once available
                         line=pass_stmt.line,
                         column=pass_stmt.column,
                     ))
+    return problems
+
+
+def _expression_not_assigned_check(parse_tree):
+    problems = []
+    for expr_stmt in parse_tree.find_data('expr_stmt'):
+        expr = expr_stmt.children[0]
+        child = expr.children[0]
+        if not isinstance(child, Tree) or child.data not in [
+                'assnmnt_expr',
+                'standalone_call',
+                'getattr_call',
+        ]:
+            problems.append(Problem(
+                name='expression-not-assigned',
+                description='expression is not asigned, and hence it can be removed',
+                line=child.line,
+                column=child.column,
+            ))
     return problems
 
 
