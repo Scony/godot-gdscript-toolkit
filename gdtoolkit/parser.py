fb930006ab6237018c4c23c0e07e33ef69e3b00c
Pawel Lampe <pawel.lampe@gmail.com>
2019-12-21 15:47:56 +0100

    speedup: lazy parser loading

--- gdtoolkit/parser.py
+++ gdtoolkit/parser.py
@@ -5,13 +5,32 @@ from pathlib import Path
 from lark import Lark, indenter
 
 
+SELF_DIR = os.path.dirname(os.path.abspath(Path(__file__).resolve()))
+
+
 def parse(code, gather_metadata=False): # TODO: lazy parser loading
     """gdscript parsing function - when gather_metadata is True
        the parsing will be slower but the tree will contain metadata
        like line and column positions for rules and tokens"""
+    global _parser, _parser_with_metadata_gathering
     code += '\n'
     if gather_metadata:
+        if _parser_with_metadata_gathering is None:
+            _parser_with_metadata_gathering = Lark.open(
+                os.path.join(SELF_DIR, 'gdscript.lark'),
+                postlex=Indenter(),
+                parser='lalr',
+                start='start',
+                propagate_positions=True
+            )
         return _parser_with_metadata_gathering.parse(code)
+    if _parser is None:
+        _parser = Lark.open(
+            os.path.join(SELF_DIR, 'gdscript.lark'),
+            postlex=Indenter(),
+            parser='lalr',
+            start='start'
+        )
     return _parser.parse(code)
 
 
@@ -24,22 +43,5 @@ class Indenter(indenter.Indenter):
     tab_len = 4                 # TODO: guess
 
 
-_self_dir = os.path.dirname(os.path.abspath(Path(__file__).resolve()))
-
-
-# fast parser, just for tree building and checking syntax
-_parser = Lark.open(
-    os.path.join(_self_dir, 'gdscript.lark'),
-    postlex=Indenter(),
-    parser='lalr',
-    start='start'
-)
-
-# slower parser, gathering metadata for static analysis
-_parser_with_metadata_gathering = Lark.open(
-    os.path.join(_self_dir, 'gdscript.lark'),
-    postlex=Indenter(),
-    parser='lalr',
-    start='start',
-    propagate_positions=True
-)
+_parser = None
+_parser_with_metadata_gathering = None
