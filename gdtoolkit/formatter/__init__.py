0e7081cce607b0aabbd3cb95367f7ca511001d78
Pawel Lampe <pawel.lampe@gmail.com>
2020-01-01 15:37:11 +0100

    formatter to end file with newline

--- gdtoolkit/formatter/__init__.py
+++ gdtoolkit/formatter/__init__.py
@@ -1,4 +1,12 @@
+from ..parser import parser
+
+
 def format_code(gdscript_code: str, max_line_length: int) -> str:
     assert max_line_length > 0
-    assert isinstance(gdscript_code, str)
-    return ""
+    parse_tree = parser.parse(gdscript_code, gather_metadata=True)
+    formatted_lines = []
+    for x in parse_tree.children:
+        if x.data == "tool_stmt":
+            formatted_lines.append("tool")
+    formatted_lines.append("")
+    return "\n".join(formatted_lines)
--- tests/formatter/test_basics.py
+++ tests/formatter/test_basics.py
@@ -4,10 +4,19 @@ from gdtoolkit.parser import parser
 from gdtoolkit.formatter import format_code
 
 
-@pytest.mark.parametrize("input_code,expected_output_code", [("", ""),])
+# fmt: off
+@pytest.mark.parametrize("input_code,expected_output_code", [
+("", ""),
+(
+"tool",
+"""tool
+""",
+),
+])
+# fmt: on
 def test_formatting(input_code, expected_output_code):
     input_code_parse_tree = parser.parse(input_code)
-    formatted_code = format_code(input_code, 100)
+    formatted_code = format_code(input_code, max_line_length=100)
     formatted_code_parse_tree = parser.parse(formatted_code)
     assert input_code_parse_tree == formatted_code_parse_tree
     assert formatted_code == expected_output_code
