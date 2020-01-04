ce8a54b709120684af792ab05625493a5f91196f
Pawel Lampe <pawel.lampe@gmail.com>
2020-01-05 00:35:31 +0100

    recursive class formatting vol. 1

--- gdtoolkit/formatter/__init__.py
+++ gdtoolkit/formatter/__init__.py
@@ -1,6 +1,17 @@
 from typing import List
 
+from lark import Tree
+
 from ..parser import parser
+
+
+class Context:
+    def __init__(
+        self, indent: int, previously_processed_line_number: int, comments: List
+    ):
+        self.indent = indent
+        self.previously_processed_line_number = previously_processed_line_number
+        self.comments = comments
 
 
 def format_code(gdscript_code: str, max_line_length: int) -> str:
@@ -9,6 +20,7 @@ def format_code(gdscript_code: str, max_line_length: int) -> str:
     comments = _gather_comments_from_code(gdscript_code)
     formatted_lines = []
     previously_processed_line_number = 0
+    # TODO: 2) replace w/ _format_class_body()
     for statement in parse_tree.children:
         formatted_lines += _reconstruct_blank_lines_in_range(
             previously_processed_line_number, statement.line, comments
@@ -16,6 +28,19 @@ def format_code(gdscript_code: str, max_line_length: int) -> str:
         previously_processed_line_number = statement.line
         if statement.data == "tool_stmt":
             formatted_lines.append("tool")
+        elif statement.data == "class_def":
+            name = statement.children[0].value
+            formatted_lines.append("class {}:".format(name))
+            class_lines, last_processed_line = _format_class_body(
+                statement.children[1:],
+                Context(
+                    indent=4,
+                    previously_processed_line_number=previously_processed_line_number,
+                    comments=comments,
+                ),
+            )
+            formatted_lines += class_lines
+            previously_processed_line_number = last_processed_line
         if comments[statement.line] is not None:
             formatted_lines[-1] = "{}  {}".format(
                 formatted_lines[-1], comments[statement.line]
@@ -25,6 +50,40 @@ def format_code(gdscript_code: str, max_line_length: int) -> str:
     )
     formatted_lines.append("")
     return "\n".join(formatted_lines)
+
+
+def _format_class_body(statements: List, context: Context) -> (List[str], int):
+    formatted_lines = []
+    previously_processed_line_number = context.previously_processed_line_number
+    for statement in statements:
+        formatted_lines += _reconstruct_blank_lines_in_range(
+            previously_processed_line_number,
+            statement.line,
+            context.comments,
+            " " * context.indent,
+        )
+        previously_processed_line_number = statement.line
+        if statement.data == "tool_stmt":
+            formatted_lines.append("{}tool".format(" " * context.indent))
+        elif statement.data == "class_def":
+            name = statement.children[0].value
+            formatted_lines.append("{}class {}:".format(" " * context.indent, name))
+            class_lines, last_processed_line = _format_class_body(
+                statement.children[1:],
+                Context(
+                    indent=context.indent + 4,
+                    previously_processed_line_number=previously_processed_line_number,
+                    comments=context.comments,
+                ),
+            )
+            formatted_lines += class_lines
+            previously_processed_line_number = last_processed_line
+        if context.comments[statement.line] is not None:
+            formatted_lines[-1] = "{}  {}".format(
+                formatted_lines[-1], context.comments[statement.line]
+            )
+    # TODO: 1) reconstruct trailing blank lines & comments
+    return (formatted_lines, previously_processed_line_number)
 
 
 def _gather_comments_from_code(gdscript_code: str) -> List[str]:
@@ -37,13 +96,14 @@ def _gather_comments_from_code(gdscript_code: str) -> List[str]:
     return comments
 
 
+# TODO: use Context
 def _reconstruct_blank_lines_in_range(
-    begin: int, end: int, comments: List[str]
+    begin: int, end: int, comments: List[str], prefix: str = ""
 ) -> List[str]:
     comments_in_range = (
         comments[begin + 1 : end] if end != -1 else comments[begin + 1 :]
     )
-    reconstructed_lines = ["" if c is None else c for c in comments_in_range]
+    reconstructed_lines = ["" if c is None else prefix + c for c in comments_in_range]
     if begin == 0:
         reconstructed_lines = _remove_empty_strings_from_begin(reconstructed_lines)
     if end == -1:
@@ -55,7 +115,7 @@ def _reconstruct_blank_lines_in_range(
     return reconstructed_lines
 
 
-def _remove_empty_strings_from_begin(lst: list) -> list:
+def _remove_empty_strings_from_begin(lst: List) -> List:
     for i, el in enumerate(lst):
         if el != "":
             return lst[i:]
--- tests/formatter/common.py
+++ tests/formatter/common.py
@@ -6,8 +6,13 @@ def format_and_compare(input_code, expected_output_code):
     input_code_parse_tree = parser.parse(input_code)
     formatted_code = format_code(input_code, max_line_length=100)
     formatted_code_parse_tree = parser.parse(formatted_code)
-    assert input_code_parse_tree == formatted_code_parse_tree
+    _invariant_check(input_code_parse_tree, formatted_code_parse_tree)
     _compare(formatted_code, expected_output_code)
+
+
+def _invariant_check(input_code_parse_tree, formatted_code_parse_tree):
+    """dummy function for better stack trace"""
+    assert input_code_parse_tree == formatted_code_parse_tree
 
 
 def _compare(formatted_code, expected_output_code):
