947933c39f2e049b3caef67bc626ef80170e21dc
Pawel Lampe <pawel.lampe@gmail.com>
2020-01-05 00:35:31 +0100

    recursive class formatting vol. 1

---
+++ tests/formatter/test_simple_nested_class_statements.py
@@ -0,0 +1,107 @@
+import pytest
+
+from .common import format_and_compare
+
+
+# fmt: off
+@pytest.mark.parametrize("input_code,expected_output_code", [
+(
+"""tool
+class X:
+    tool
+""",
+"""tool
+class X:
+    tool
+""",
+),
+(
+"""tool
+tool
+class X:
+    tool
+    tool
+""",
+"""tool
+tool
+class X:
+    tool
+    tool
+""",
+),
+(
+"""tool
+tool
+class X:
+    tool    # xxx
+    tool
+""",
+"""tool
+tool
+class X:
+    tool  # xxx
+    tool
+""",
+),
+(
+"""tool
+tool
+class X:
+    tool    # xxx
+
+
+    tool
+""",
+"""tool
+tool
+class X:
+    tool  # xxx
+
+
+    tool
+""",
+),
+(
+"""tool
+class X:
+    tool
+    class Y:
+        tool
+""",
+"""tool
+class X:
+    tool
+    class Y:
+        tool
+""",
+),
+(
+"""tool
+class X:
+    # aaa
+    tool
+""",
+"""tool
+class X:
+    # aaa
+    tool
+""",
+),
+# (
+# """tool
+# class X:
+#     # aaa
+#     tool
+#     # bbb
+# """,
+# """tool
+# class X:
+#     # aaa
+#     tool
+#     # bbb
+# """,
+# ),
+])
+# fmt: on
+def test_formatting(input_code, expected_output_code):
+    format_and_compare(input_code, expected_output_code)
