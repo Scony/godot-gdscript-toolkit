b9c512cc279e39bc679903289c7d1f3e72a72a35
Pawel Lampe <pawel.lampe@gmail.com>
2020-01-14 22:23:01 +0100

    simple atomic expressions formatting, vol. 2

--- gdtoolkit/formatter/expression.py
+++ gdtoolkit/formatter/expression.py
@@ -3,15 +3,27 @@ from typing import List, Tuple
 from lark import Tree
 
 from .context import Context
+from .types import Prefix, Node
 
 
 def format_expression(
-    prefix: str, expression: Tree, context: Context
+    prefix: Prefix, expression: Tree, context: Context
+) -> Tuple[List, int]:
+    return _format_concrete_expression(prefix, expression.children[0], context)
+
+
+def _format_concrete_expression(
+    prefix: Prefix, expression: Node, context: Context
 ) -> Tuple[List, int]:
     assert context
     formatted_lines = []
-    # TODO: take original line from prefix
     formatted_lines.append(
-        (expression.line, "{}{}".format(prefix, expression.children[0].value))
+        (prefix.line, "{}{}".format(prefix.string, _format_atom(expression)))
     )
     return (formatted_lines, expression.line)
+
+
+def _format_atom(atom: Node) -> str:
+    if isinstance(atom, Tree):
+        return atom.children[0].value
+    return atom.value
--- tests/formatter/input-output-pairs/simple-atom-expressions.in.gd
+++ tests/formatter/input-output-pairs/simple-atom-expressions.in.gd
@@ -3,3 +3,5 @@ class X:
 		var x=1
 		var y = x
 		var z = 0x99
+		var q = null
+		var w = 'xxx'
