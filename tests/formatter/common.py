a57bc769e7f2640404c9861242c981625787a4ae
Pawel Lampe <pawel.lampe@gmail.com>
2020-01-15 20:41:36 +0100

    formatting short arrays

--- tests/formatter/common.py
+++ tests/formatter/common.py
@@ -9,11 +9,13 @@ MAX_LINE_LENGTH = 100
 
 
 def format_and_compare(input_code, expected_output_code):
-    input_code_parse_tree = parser.parse(input_code, loosen_grammar=True)
     formatted_code = format_code(input_code, max_line_length=MAX_LINE_LENGTH)
+    _compare(formatted_code, expected_output_code)
+
+    input_code_parse_tree = parser.parse(input_code, loosen_grammar=True)
     formatted_code_parse_tree = parser.parse(formatted_code, loosen_grammar=True)
     _invariant_check(input_code_parse_tree, formatted_code_parse_tree)
-    _compare(formatted_code, expected_output_code)
+
     input_code_comment_stats = _gather_comment_statistics_from_code(input_code)
     formatted_code_comments = _gather_comments_from_code(formatted_code)
     _comment_preservation_check(input_code_comment_stats, formatted_code_comments)
