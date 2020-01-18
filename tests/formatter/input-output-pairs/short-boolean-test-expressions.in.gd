f3611e5ceb3d6594372f9448cdf3d48e47a3fb2d
Pawel Lampe <pawel.lampe@gmail.com>
2020-01-18 19:33:33 +0100

    formatting foldable expressions vol. 4

---
+++ tests/formatter/input-output-pairs/short-boolean-test-expressions.in.gd
@@ -0,0 +1,10 @@
+class X:
+	func foo():
+		var a = 1 > 2
+		var b = "xxxxxxxxxxxxxxxxxxxxxxxxxxx" > "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
+		var c = [1, 2] in [[1, 2], [3]]
+		var d = not not not true
+		var e = true and true && true
+		var f = true or true || true
+		var g = 1 if true else 0
+		var h = (1+1)+1
