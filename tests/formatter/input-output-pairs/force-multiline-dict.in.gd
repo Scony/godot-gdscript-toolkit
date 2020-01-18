db67ecf85e9c5cf1d6af564cb4296ad1b252f951
Pawel Lampe <pawel.lampe@gmail.com>
2020-01-18 12:57:28 +0100

    dicts formatting vol. 2, closes #29

---
+++ tests/formatter/input-output-pairs/force-multiline-dict.in.gd
@@ -0,0 +1,7 @@
+class X:
+	func foo():
+		var x = {'a': 1, a = 1, 'a': 1,}
+		var y = {
+			[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+			xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+		}
---
+++ tests/formatter/input-output-pairs/force-multiline-dict.out.gd
@@ -0,0 +1,13 @@
+class X:
+	func foo():
+		var x = {
+			'a': 1,
+			a = 1,
+			'a': 1,
+		}
+		var y = {
+			[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]:
+			[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+			xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx =
+			[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+		}
