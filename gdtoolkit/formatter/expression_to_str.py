42447a423833b4c144624b5c84f44cc8ad2e9e6b
Pawel Lampe <pawel.lampe@gmail.com>
2020-01-18 14:59:31 +0100

    formatter refactoring

--- gdtoolkit/formatter/expression_to_str.py
+++ gdtoolkit/formatter/expression_to_str.py
@@ -1,45 +1,43 @@
 from lark import Tree
 
 from .types import Node
-from .expression_utils import is_foldable
 
 
 def expression_to_str(expression: Node) -> str:
-    if is_foldable(expression):
-        return foldable_to_str(expression)
-    return non_foldable_to_str(expression)
-
-
-def foldable_to_str(expression: Node) -> str:
-    if expression.data == "array":
-        array_elements = [
-            expression_to_str(child)
-            for child in expression.children
-            if isinstance(child, Tree) or child.type != "COMMA"
-        ]
-        return "[{}]".format(", ".join(array_elements))
-    if expression.data == "dict":
-        elements = [_dict_element_to_str(child) for child in expression.children]
-        return "{{{}}}".format(", ".join(elements))
-    if expression.data == "subscr_expr":
-        return "{}[{}]".format(
-            expression_to_str(expression.children[0]),
-            expression_to_str(expression.children[1]),
-        )
-    return ""
-
-
-def non_foldable_to_str(expression: Node) -> str:
     if isinstance(expression, Tree):
-        if expression.data == "string":
-            return expression.children[0].value
-        if expression.data == "node_path":
-            return "{}{}".format("@", non_foldable_to_str(expression.children[0]))
-        if expression.data == "get_node":
-            return "{}{}".format("$", non_foldable_to_str(expression.children[0]))
-        if expression.data == "path":
-            return "/".join([name_token.value for name_token in expression.children])
+        return {
+            "subscr_expr": _subscription_to_str,
+            "array": _array_to_str,
+            "dict": _dict_to_str,
+            "c_dict_element": _dict_element_to_str,
+            "eq_dict_element": _dict_element_to_str,
+            "string": lambda e: e.children[0].value,
+            "node_path": lambda e: "@{}".format(expression_to_str(e.children[0])),
+            "get_node": lambda e: "${}".format(expression_to_str(e.children[0])),
+            "path": lambda e: "/".join([name_token.value for name_token in e.children]),
+        }[expression.data](expression)
     return expression.value
+
+
+def _array_to_str(array: Tree) -> str:
+    elements = [
+        expression_to_str(child)
+        for child in array.children
+        if isinstance(child, Tree) or child.type != "COMMA"
+    ]
+    return "[{}]".format(", ".join(elements))
+
+
+def _dict_to_str(a_dict: Tree) -> str:
+    elements = [expression_to_str(child) for child in a_dict.children]
+    return "{{{}}}".format(", ".join(elements))
+
+
+def _subscription_to_str(subscription: Tree) -> str:
+    return "{}[{}]".format(
+        expression_to_str(subscription.children[0]),
+        expression_to_str(subscription.children[1]),
+    )
 
 
 def _dict_element_to_str(dict_element: Tree) -> str:
