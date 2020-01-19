7d9c8afc760f3a8f0491823409ea453ca2717e3e
Pawel Lampe <pawel.lampe@gmail.com>
2020-01-19 13:28:31 +0100

    formatter exec (not published yet), closes #34

---
+++ gdtoolkit/formatter/__main__.py
@@ -0,0 +1,34 @@
+"""GDScript formatter, NOT SUITABLE FOR PRODUCTION YET!
+
+...
+
+Usage:
+  gdformat <file>... [options]
+
+Options:
+  -h --help                  Show this screen.
+  --version                  Show version.
+"""
+import sys
+import pkg_resources
+
+from docopt import docopt
+
+from gdtoolkit.formatter import format_code
+
+
+def main():
+    arguments = docopt(
+        __doc__,
+        version="gdformat {}".format(
+            pkg_resources.get_distribution("gdtoolkit").version
+        ),
+    )
+    if arguments["<file>"] == ["-"]:
+        code = sys.stdin.read()
+        print(format_code(gdscript_code=code, max_line_length=100), end="")
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
