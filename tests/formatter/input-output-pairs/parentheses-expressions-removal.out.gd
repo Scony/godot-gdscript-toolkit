713a0617f124ffe3540132805d9c83a883476b16
Pawel Lampe <pawel.lampe@gmail.com>
2020-02-01 18:41:35 +0100

    parentheses formatting bugfix

--- tests/formatter/input-output-pairs/parentheses-expressions-removal.out.gd
+++ tests/formatter/input-output-pairs/parentheses-expressions-removal.out.gd
@@ -1,3 +1,16 @@
 class X:
-	func foo():
+	func foo(p):
 		var x = 1
+		var y = foo(1)
+		var z = foo(
+			"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+		)
+		var a = [1, 2]
+		var b = {1: 2}
+		var c = [
+			"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+			"x"
+		]
+		var d = {
+			"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+		}
