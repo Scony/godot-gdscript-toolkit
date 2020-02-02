86312daf173593f4b0e9ccdb0f7243405ed8b43d
Pawel Lampe <pawel.lampe@gmail.com>
2020-02-02 17:51:27 +0100

    improved export formatting

--- tests/formatter/input-output-pairs/setget.out.gd
+++ tests/formatter/input-output-pairs/setget.out.gd
@@ -1,7 +1,7 @@
 class X:
-	export(int) var a setget sss, ggg
-	export(int) var b setget sss
-	export(int) var c setget , ggg
+	export (int) var a setget sss, ggg
+	export (int) var b setget sss
+	export (int) var c setget , ggg
 	var x setget sss, ggg
 	var y setget sss
 	var z setget , ggg
