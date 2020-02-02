9f2de0f55de45cb70f4672685554dac76567bc6e
Pawel Lampe <pawel.lampe@gmail.com>
2020-02-02 14:43:09 +0100

    formatter tc fix

--- tests/formatter/input-output-pairs/simple-atom-expressions.out.gd
+++ tests/formatter/input-output-pairs/simple-atom-expressions.out.gd
@@ -4,10 +4,11 @@ extends Node
 func foo():
 	var x = 1
 	var y = x
-	var z = 0x99
+	var z = 0x99ff
 	var q = null
 	var w = 'xxx'
 	var e = @"../Node"
 	var r = """asd"""
 	var t = $Some/Path
 	var k = $"../Some/Stuff"
+	var l = 0xfF9900
