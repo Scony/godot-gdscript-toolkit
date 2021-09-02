88e743c8749a6ff729c26fff6ac100825402fcac
Pawel Lampe <pawel.lampe@gmail.com>
2021-09-02 22:16:30 +0200

    generated testcases for expression formatting

--- tests/generated/test_expression_parsing.py
+++ tests/generated/test_expression_parsing.py
@@ -1,12 +1,13 @@
-import os
-
 import pytest
 import hypothesis.extra.lark
 from hypothesis import settings, given, HealthCheck
 from lark import Lark
 
 from gdtoolkit.parser import parser
+from gdtoolkit.formatter import format_code, check_formatting_safety
 
+
+MAX_LINE_LENGTH = 100
 
 with open("gdtoolkit/parser/gdscript.lark", "r") as fh:
     gdscript_grammar = fh.read()
@@ -24,6 +25,11 @@ with open("gdtoolkit/parser/gdscript.lark", "r") as fh:
     gdscript_lark = Lark(simplified_gdscript_grammar)
 
 
+def format_and_check_safety(input_code):
+    formatted_code = format_code(input_code, max_line_length=MAX_LINE_LENGTH)
+    check_formatting_safety(input_code, formatted_code, MAX_LINE_LENGTH)
+
+
 @pytest.mark.generated
 @settings(
     deadline=None,
@@ -35,3 +41,16 @@ def test_expression_parsing(expression):
     print(expression)
     gdscript_code = "func foo():{}".format(expression)
     parser.parse(gdscript_code)  # just checking if not throwing
+
+
+@pytest.mark.generated
+@settings(
+    deadline=None,
+    suppress_health_check=(HealthCheck.filter_too_much,),
+    max_examples=500,
+)
+@given(hypothesis.extra.lark.from_lark(gdscript_lark, start="expr"))
+def test_expression_formatting(expression):
+    print(expression)
+    gdscript_code = "func foo():{}".format(expression)
+    format_and_check_safety(gdscript_code)
