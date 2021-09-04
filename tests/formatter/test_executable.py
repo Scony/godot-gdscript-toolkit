6ed4c241e6448bdfbcb7b760ae283f84d956a8e6
LoipesMas <46327403+LoipesMas@users.noreply.github.com>
2021-09-04 18:14:09 +0200

    Add test for --check for valid files

--- tests/formatter/test_executable.py
+++ tests/formatter/test_executable.py
@@ -8,6 +8,13 @@ def test_valid_file(tmp_path):
     assert subprocess.run(["gdformat", dummy_file], check=False).returncode == 0
 
 
+def test_check_valid_file_unchanged(tmp_path):
+    dummy_file = write_file(tmp_path, "script.gd", "tool\n")
+    assert (
+        subprocess.run(["gdformat", "--check", dummy_file], check=False).returncode == 0
+    )
+
+
 def test_check_valid_file_to_reformat(tmp_path):
     dummy_file = write_file(tmp_path, "script.gd", "tool;var x")
     assert (
