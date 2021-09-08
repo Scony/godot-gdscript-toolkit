2a83208137818c7dce08a616a23240fc8b5b4d1b
Pawel Lampe <pawel.lampe@gmail.com>
2021-09-08 18:28:10 +0200

    finished exception pretty-printing in gdparse, #2

--- gdtoolkit/parser/__main__.py
+++ gdtoolkit/parser/__main__.py
@@ -14,12 +14,16 @@ Options:
 """
 import sys
 import pkg_resources
+from typing import Dict
 
 import lark
 from docopt import docopt
 
 from gdtoolkit.parser import parser
-from gdtoolkit.common.exceptions import lark_unexpected_input_to_str
+from gdtoolkit.common.exceptions import (
+    lark_unexpected_token_to_str,
+    lark_unexpected_input_to_str,
+)
 
 
 def main():
@@ -34,21 +38,22 @@ def main():
         print(arguments)
         sys.exit(0)
 
-    success = True
     files = arguments["<file>"]
+
+    success = True
 
     if files == ["-"]:
         file_content = sys.stdin.read()
         success = _parse_file_content(file_content, arguments)
     else:
-        for file_path in arguments["<file>"]:
+        for file_path in files:
             success &= _parse_file(file_path, arguments)
 
     if not success:
         sys.exit(1)
 
 
-def _parse_file(file_path, arguments):
+def _parse_file(file_path: str, arguments: Dict):
     try:
         with open(file_path, "r") as fh:
             file_content = fh.read()
@@ -61,21 +66,31 @@ def _parse_file(file_path, arguments):
     return False
 
 
-def _parse_file_content(content, arguments, file_path=None):
+def _parse_file_content(content: str, arguments: Dict, file_path: str = None):
     actual_file_path = "STDIN" if file_path is None else file_path
     try:
         tree = parser.parse(content)  # TODO: handle exceptions
+    except lark.exceptions.UnexpectedToken as e:
+        print(
+            f"{actual_file_path}:\n",
+            lark_unexpected_token_to_str(e, content),
+            sep="\n",
+            file=sys.stderr,
+        )
+        return False
     except lark.exceptions.UnexpectedInput as e:
         print(
             f"{actual_file_path}:\n",
-            lark_unexpected_input_to_str(e, content),
+            lark_unexpected_input_to_str(e),
             sep="\n",
             file=sys.stderr,
         )
         return False
     if arguments["--pretty"]:
+        print(f"{actual_file_path}:\n")
         print(tree.pretty())
     elif arguments["--verbose"]:
+        print(f"{actual_file_path}:\n")
         print(tree)
     return True
 
