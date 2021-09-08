3bceb54dd759673423f84e1338849915bbd7c3ba
Pawel Lampe <pawel.lampe@gmail.com>
2021-09-08 19:58:01 +0200

    refa, extracting common stuff to utils

---
+++ gdtoolkit/common/utils.py
@@ -0,0 +1,21 @@
+import os
+from typing import Set, List
+
+Path = str
+
+
+def find_gd_files_from_paths(
+    paths: List[Path], excluded_directories: Set[Path]
+) -> List[Path]:
+    """Finds .gd files in directories recursively and combines results to the list"""
+    files = []
+    for path in paths:
+        if os.path.isdir(path):
+            for dirpath, dirnames, filenames in os.walk(path, topdown=True):
+                dirnames[:] = [d for d in dirnames if d not in excluded_directories]
+                files += [
+                    os.path.join(dirpath, f) for f in filenames if f.endswith(".gd")
+                ]
+        else:
+            files.append(path)
+    return files
