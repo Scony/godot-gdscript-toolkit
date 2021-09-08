9f9fa7a8e1aed7fc62f1250d78795554a7a7f9a9
Pawel Lampe <pawel.lampe@gmail.com>
2021-09-08 18:28:10 +0200

    finished exception pretty-printing in gdparse, #2

--- gdtoolkit/common/exceptions.py
+++ gdtoolkit/common/exceptions.py
@@ -2,5 +2,11 @@ import lark
 
 
 # TODO: use in formatter/linter
-def lark_unexpected_input_to_str(exception: lark.exceptions.UnexpectedInput, code):
-    return f"{exception.get_context(code)}{exception}".strip()
+
+
+def lark_unexpected_input_to_str(exception: lark.exceptions.UnexpectedInput):
+    return str(exception).strip()
+
+
+def lark_unexpected_token_to_str(exception: lark.exceptions.UnexpectedToken, code: str):
+    return f"{exception.get_context(code)}\n{exception}".strip()
--- tests/parser/test_executable.py
+++ tests/parser/test_executable.py
@@ -11,12 +11,35 @@ def test_parsing_valid_file_succeeds(tmp_path):
     assert len(outcome.stderr.decode().splitlines()) == 0
 
 
+def test_parsing_valid_files_succeeds(tmp_path):
+    dummy_file = write_file(tmp_path, "script.gd", "tool")
+    dummy_file2 = write_file(tmp_path, "script2.gd", "pass;pass")
+    outcome = subprocess.run(
+        ["gdparse", dummy_file, dummy_file2], check=False, capture_output=True
+    )
+    assert outcome.returncode == 0
+    assert len(outcome.stdout.decode().splitlines()) == 0
+    assert len(outcome.stderr.decode().splitlines()) == 0
+
+
+def test_pretty_printing_valid_files_succeeds(tmp_path):
+    dummy_file = write_file(tmp_path, "script.gd", "tool")
+    dummy_file2 = write_file(tmp_path, "script2.gd", "pass;pass")
+    outcome = subprocess.run(
+        ["gdparse", "-p", dummy_file, dummy_file2], check=False, capture_output=True
+    )
+    assert outcome.returncode == 0
+    assert len(outcome.stdout.decode().splitlines()) > 0
+    assert len(outcome.stderr.decode().splitlines()) == 0
+
+
 def test_pretty_printing_missing_file():
     outcome = subprocess.run(
         ["gdparse", "nonexistent.gd"], check=False, capture_output=True
     )
     assert outcome.returncode == 1
     assert len(outcome.stdout.decode().splitlines()) == 0
+    assert len(outcome.stderr.decode().splitlines()) > 0
     assert "Traceback" not in outcome.stderr.decode()
 
 
@@ -25,4 +48,6 @@ def test_pretty_printing_unexpected_token():
         ["gdparse", "-"], input=b"pass x", check=False, capture_output=True
     )
     assert outcome.returncode == 1
+    assert len(outcome.stdout.decode().splitlines()) == 0
+    assert len(outcome.stderr.decode().splitlines()) > 0
     assert "Traceback" not in outcome.stderr.decode()
