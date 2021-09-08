7932c77ef0f5edd294cf042b539807306e4ab045
Pawel Lampe <pawel.lampe@gmail.com>
2021-09-08 22:19:33 +0200

    formatter keeps going despite errors, fixes #121

--- tests/formatter/test_executable.py
+++ tests/formatter/test_executable.py
@@ -22,8 +22,33 @@ def test_valid_files_formatting(tmp_path):
     assert len(outcome.stderr.decode().splitlines()) == 0
 
 
-# TODO: test_valid_files_formatting_with_nonexistent_one_keepgoing
-# TODO: test_valid_files_formatting_with_invalid_one_keepgoing
+def test_valid_files_formatting_with_nonexistent_one_keepgoing(tmp_path):
+    dummy_file = write_file(tmp_path, "script.gd", "pass")
+    dummy_file_3 = write_file(tmp_path, "script3.gd", "pass;pass")
+    outcome = subprocess.run(
+        ["gdformat", dummy_file, "nonexistent.gd", dummy_file_3],
+        check=False,
+        capture_output=True,
+    )
+    assert outcome.returncode == 1
+    assert len(outcome.stdout.decode().splitlines()) == 3
+    assert len(outcome.stderr.decode().splitlines()) > 0
+    assert "Traceback" not in outcome.stderr.decode()
+
+
+def test_valid_files_formatting_with_invalid_one_keepgoing(tmp_path):
+    dummy_file = write_file(tmp_path, "script.gd", "pass")
+    dummy_file_2 = write_file(tmp_path, "script2.gd", "pass x")  # invalid
+    dummy_file_3 = write_file(tmp_path, "script3.gd", "pass;pass")
+    outcome = subprocess.run(
+        ["gdformat", dummy_file, dummy_file_2, dummy_file_3],
+        check=False,
+        capture_output=True,
+    )
+    assert outcome.returncode == 1
+    assert len(outcome.stdout.decode().splitlines()) == 3
+    assert len(outcome.stderr.decode().splitlines()) > 0
+    assert "Traceback" not in outcome.stderr.decode()
 
 
 def test_valid_formatted_file_checking(tmp_path):
@@ -46,5 +71,45 @@ def test_valid_unformatted_file_checking(tmp_path):
     assert len(outcome.stderr.decode().splitlines()) == 2
 
 
-# TODO: test_valid_unformatted_files_checking_with_nonexistent_one_keepgoing
-# TODO: test_valid_unformatted_files_checking_with_invalid_one_keepgoing
+def test_valid_unformatted_files_checking_with_invalid_one_keepgoing(tmp_path):
+    dummy_file = write_file(tmp_path, "script.gd", "pass")
+    dummy_file_2 = write_file(tmp_path, "script2.gd", "pass x")  # invalid
+    dummy_file_3 = write_file(tmp_path, "script3.gd", "pass;pass")
+    outcome = subprocess.run(
+        ["gdformat", "--check", dummy_file, dummy_file_2, dummy_file_3],
+        check=False,
+        capture_output=True,
+    )
+    assert outcome.returncode == 1
+    assert len(outcome.stdout.decode().splitlines()) == 0
+    assert len(outcome.stderr.decode().splitlines()) > 0
+    assert "Traceback" not in outcome.stderr.decode()
+
+
+def test_valid_formatted_files_checking_with_nonexistent_one_keepgoing(tmp_path):
+    dummy_file = write_file(tmp_path, "script.gd", "pass\n")
+    dummy_file_3 = write_file(tmp_path, "script3.gd", "pass\n")
+    outcome = subprocess.run(
+        ["gdformat", "--check", dummy_file, "nonexistent.gd", dummy_file_3],
+        check=False,
+        capture_output=True,
+    )
+    assert outcome.returncode == 1
+    assert len(outcome.stdout.decode().splitlines()) == 1
+    assert len(outcome.stderr.decode().splitlines()) > 0
+    assert "Traceback" not in outcome.stderr.decode()
+
+
+def test_valid_formatted_files_checking_with_invalid_one_keepgoing(tmp_path):
+    dummy_file = write_file(tmp_path, "script.gd", "pass\n")
+    dummy_file_2 = write_file(tmp_path, "script2.gd", "pass x")  # invalid
+    dummy_file_3 = write_file(tmp_path, "script3.gd", "pass\n")
+    outcome = subprocess.run(
+        ["gdformat", "--check", dummy_file, dummy_file_2, dummy_file_3],
+        check=False,
+        capture_output=True,
+    )
+    assert outcome.returncode == 1
+    assert len(outcome.stdout.decode().splitlines()) == 1
+    assert len(outcome.stderr.decode().splitlines()) > 0
+    assert "Traceback" not in outcome.stderr.decode()
