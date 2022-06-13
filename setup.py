02ed8eb4ea8e4ec859f44b4ee6046bf8ca926816
vince.joe <vince.joe@kakaocorp.com>
2022-06-13 23:47:14 +0900

    Add python module topic

--- setup.py
+++ setup.py
@@ -46,5 +46,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
