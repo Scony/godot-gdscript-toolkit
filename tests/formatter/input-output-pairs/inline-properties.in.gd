b74849a9bd8dbbf53dbd59a491e5141657c937ca
Pawel Lampe <pawel.lampe@gmail.com>
2022-09-25 22:51:44 +0200

    add missing formatter test cases for inline-properties, #154

--- tests/formatter/input-output-pairs/inline-properties.in.gd
+++ tests/formatter/input-output-pairs/inline-properties.in.gd
@@ -5,7 +5,19 @@ var p4: get = __get, set = __set # TODO: add newline after to trigger stability 
 var p5 = 1: set = __set
 var p6 = 1: set = __set, get = __get
 var p7 = 1: get = __get
-var p8 = 1: get = __get, set = __set
+var p8 = 1: get = __get, set = __set # TODO:
+var p9 : int: set = __set
+var p10 : int: set = __set, get = __get
+var p11 : int: get = __get
+var p12 : int: get = __get, set = __set # TODO:
+var p13 : int = 1: set = __set
+var p14 : int = 1: set = __set, get = __get
+var p15 : int = 1: get = __get
+var p16 : int = 1: get = __get, set = __set # TODO:
+var p17 := 1: set = __set
+var p18 := 1: set = __set, get = __get
+var p19 := 1: get = __get
+var p20 := 1: get = __get, set = __set
 
 func __get():
 	return 1
--- tests/formatter/input-output-pairs/inline-properties.out.gd
+++ tests/formatter/input-output-pairs/inline-properties.out.gd
@@ -17,6 +17,36 @@ var p7 = 1:
 	get = __get
 var p8 = 1:
 	get = __get,
+	set = __set  # TODO:
+var p9: int:
+	set = __set
+var p10: int:
+	set = __set,
+	get = __get
+var p11: int:
+	get = __get
+var p12: int:
+	get = __get,
+	set = __set  # TODO:
+var p13: int = 1:
+	set = __set
+var p14: int = 1:
+	set = __set,
+	get = __get
+var p15: int = 1:
+	get = __get
+var p16: int = 1:
+	get = __get,
+	set = __set  # TODO:
+var p17 := 1:
+	set = __set
+var p18 := 1:
+	set = __set,
+	get = __get
+var p19 := 1:
+	get = __get
+var p20 := 1:
+	get = __get,
 	set = __set
 
 
