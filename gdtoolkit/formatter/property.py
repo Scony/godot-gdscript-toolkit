40d70922cc7a713fa1029c6273bfd14c9d5bba03
Pawel Lampe <pawel.lampe@gmail.com>
2022-09-28 23:24:00 +0200

    harness block formatter to properties formatting, #154

--- gdtoolkit/formatter/property.py
+++ gdtoolkit/formatter/property.py
@@ -1,10 +1,10 @@
-from typing import List
-
 from lark import Tree
+from lark.tree import Meta
 
 from .statement_utils import find_tree_among_children
 from .types import FormattedLine, FormattedLines, Outcome
 from .context import Context
+from .block import format_block
 
 
 def has_inline_property_body(statement: Tree) -> bool:
@@ -15,64 +15,69 @@ def append_property_body_to_formatted_line(
     line_to_append_to: FormattedLine, inline_property_body: Tree, context: Context
 ) -> FormattedLines:
     formatted_lines = [(line_to_append_to[0], f"{line_to_append_to[1]}:")]
-    child_context = context.create_child_context(inline_property_body.line)
     property_delegates = inline_property_body.children
     if len(property_delegates) > 0:
-        formatted_lines += _format_property_delegates(
-            property_delegates, child_context
-        )[0]
+        fake_meta = Meta()
+        fake_meta.line = inline_property_body.children[0].line
+        fake_property_body = Tree(
+            "property_body", inline_property_body.children, fake_meta
+        )
+        extra_lines, _ = format_property_body(fake_property_body, context)
+        formatted_lines += extra_lines
     return formatted_lines
 
 
 def format_property_body(property_body: Tree, context: Context) -> Outcome:
-    child_context = context.create_child_context(property_body.line)
-    if property_body.children[0].data.startswith("property_delegate"):
-        return _format_property_delegates(property_body.children, child_context)
-    return _format_property_etters(property_body.children, child_context)
+    formatted_lines, last_processed_line = format_block(
+        property_body.children,
+        _format_property_statement,
+        context.create_child_context(property_body.line),
+    )
+    if (
+        property_body.children[0].data.startswith("property_delegate")
+        and len(property_body.children) > 1
+    ):
+        for formatted_line_index, formatted_line in enumerate(formatted_lines):
+            if formatted_line[0] == property_body.children[0].line:
+                formatted_lines[formatted_line_index] = (
+                    formatted_line[0],
+                    formatted_line[1] + ",",
+                )
+                break
+    return (formatted_lines, last_processed_line)
 
 
-def _format_property_delegates(
-    property_delegates: List[Tree], context: Context
-) -> Outcome:
-    formatted_lines = [
-        _format_property_delegate(
-            property_delegate,
-            context,
-        )
-        for property_delegate in property_delegates
-    ]
-    if len(formatted_lines) == 2:
-        formatted_lines = [
-            (
-                formatted_lines[0][0],
-                f"{formatted_lines[0][1]},",
-            ),
-            formatted_lines[1],
-        ]
-    return (formatted_lines, property_delegates[-1].line)
+def _format_property_statement(statement: Tree, context: Context) -> Outcome:
+    handlers = {
+        "property_delegate_set": _format_property_delegate,
+        "property_delegate_get": _format_property_delegate,
+        "property_custom_getter": _format_property_etter,
+    }
+    return handlers[statement.data](statement, context)
 
 
-def _format_property_etters(property_etters: List[Tree], context: Context):
-    child_context = context.create_child_context(property_etters[0].line)
+def _format_property_etter(property_etter: Tree, context: Context) -> Outcome:
+    child_context = context.create_child_context(property_etter.line)
     return (
         [
-            (property_etters[0].line, f"{context.indent_string}get:"),
-            (property_etters[0].line, f"{child_context.indent_string}pass"),
+            (property_etter.line, f"{context.indent_string}get:"),
+            (property_etter.line, f"{child_context.indent_string}pass"),
         ],
-        property_etters[
-            -1
-        ].end_line,  # TODO: likely last processed line from last etter
+        property_etter.line,
     )
 
 
-def _format_property_delegate(
-    property_delegate: Tree, context: Context
-) -> FormattedLine:
+def _format_property_delegate(property_delegate: Tree, context: Context) -> Outcome:
     return (
+        [
+            (
+                property_delegate.line,
+                "{}{} = {}".format(
+                    context.indent_string,
+                    property_delegate.children[0].value,
+                    property_delegate.children[2].value,
+                ),
+            )
+        ],
         property_delegate.line,
-        "{}{} = {}".format(
-            context.indent_string,
-            property_delegate.children[0].value,
-            property_delegate.children[2].value,
-        ),
     )
