64f2d288e95eb73636f00f04f55c43055d077e9d
Pawel Lampe <pawel.lampe@gmail.com>
2022-10-24 23:38:13 +0200

    extend disabled formatter testcase for complex corner-case to solve

--- tests/formatter/input-output-pairs/long-attribute-expression-corner-case.in.gd
+++ tests/formatter/input-output-pairs/long-attribute-expression-corner-case.in.gd
@@ -1,2 +1,3 @@
 func foo(x):
-		var path: String = x.result.path.substr(0, 10).replace("res://", "").replace("tests/", "").replace("", "").replace("", "").replace("", "").replace("", "").replace("", "").replace("/", " ").capitalize()
+		var path: String = x.result.path.substr(0, 10).replace("res://", "").replace("tests/", "").replace("", "").replace("", "").replace("", "").replace("", "").replace("", "").replace("/", " ").capitalize(1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28)
+		var path2 = x.aaaa.bbbb.ccccc().ddddd.eeee.ffff(1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28).gggg().hhhh.jjjj
--- tests/formatter/input-output-pairs/long-attribute-expression-corner-case.out.gd
+++ tests/formatter/input-output-pairs/long-attribute-expression-corner-case.out.gd
@@ -7,10 +7,78 @@ func foo(x):
 		. replace("res://", "")
 		. replace("tests/", "")
 		. replace("", "")
-		.replace("", "")
-		.replace("", "")
-		.replace("", "")
-		.replace("", "")
-		.replace("/", " ")
-		.capitalize()
+		. replace("", "")
+		. replace("", "")
+		. replace("", "")
+		. replace("", "")
+		. replace("/", " ")
+		. capitalize(
+			1,
+			2,
+			3,
+			4,
+			6,
+			7,
+			8,
+			9,
+			10,
+			11,
+			12,
+			13,
+			14,
+			15,
+			16,
+			17,
+			18,
+			19,
+			20,
+			21,
+			22,
+			23,
+			24,
+			25,
+			26,
+			27,
+			28
+		)
+	)
+	var path2 = (
+		x
+		. aaaa
+		. bbbb
+		. ccccc()
+		. ddddd
+		. eeee
+		. ffff(
+			1,
+			2,
+			3,
+			4,
+			6,
+			7,
+			8,
+			9,
+			10,
+			11,
+			12,
+			13,
+			14,
+			15,
+			16,
+			17,
+			18,
+			19,
+			20,
+			21,
+			22,
+			23,
+			24,
+			25,
+			26,
+			27,
+			28
+		)
+		. gggg()
+		. hhhh
+		. jjjj
 	)
