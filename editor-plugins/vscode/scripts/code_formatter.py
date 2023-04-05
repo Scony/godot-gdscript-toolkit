5f0da8f6b8483fc23a784a4c204ecf009528252c
Ryu G.H <76783667+mindnect@users.noreply.github.com>
2023-04-05 22:54:55 +0900

    Fix utf-8 character broken

--- editor-plugins/vscode/scripts/code_formatter.py
+++ editor-plugins/vscode/scripts/code_formatter.py
@@ -1,5 +1,7 @@
 import sys
+import io
 from gdtoolkit.formatter import format_code
 
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
 formatted_code = format_code(sys.argv[1], int(sys.argv[2]))
 print(formatted_code)
