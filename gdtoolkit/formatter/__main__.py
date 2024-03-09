b23ac6d5b7427ac1705e62d9257e3ab7d311fc02
Christen Lofland <christen@lofland.net>
2024-03-09 06:04:35 -0600

    Add gdformat config file for excluding folders. (#286)
    * Have gdformat read the gdlint config file for excluded folders.
    
    * Use dedicated gdformatrc file for gdformat.

--- gdtoolkit/formatter/__main__.py
+++ gdtoolkit/formatter/__main__.py
@@ -6,6 +6,7 @@ care of by gdformat in a one, consistent way.
 
 Usage:
   gdformat <path>... [options]
+  gdformat --dump-default-config
 
 Options:
   -c --check                 Don't write the files back,
@@ -19,19 +20,25 @@ Options:
   -s --use-spaces=<int>      Use spaces for indent instead of tabs.
   -h --help                  Show this screen.
   --version                  Show version.
+  --dump-default-config      Dump default config to 'gdformatrc' file.
 
 Examples:
   echo 'pass' | gdformat -   # reads from STDIN
 """
 import sys
+import os
+import logging
+import pathlib
 import difflib
 from typing import List, Tuple, Optional
+from types import MappingProxyType
 import pkg_resources
 
 from docopt import docopt
 import lark
+import yaml
 
-from gdtoolkit.formatter import format_code, check_formatting_safety
+from gdtoolkit.formatter import format_code, check_formatting_safety, DEFAULT_CONFIG
 from gdtoolkit.formatter.exceptions import (
     TreeInvariantViolation,
     FormattingStabilityViolation,
@@ -44,6 +51,8 @@ from gdtoolkit.common.exceptions import (
     lark_unexpected_input_to_str,
 )
 
+CONFIG_FILE_NAME = "gdformatrc"
+
 
 def main():
     sys.stdout.reconfigure(encoding="utf-8")
@@ -53,6 +62,9 @@ def main():
             pkg_resources.get_distribution("gdtoolkit").version
         ),
     )
+
+    if arguments["--dump-default-config"]:
+        _dump_default_config()
 
     if arguments["--diff"]:
         arguments["--check"] = True
@@ -64,8 +76,14 @@ def main():
         else None
     )
     safety_checks = not arguments["--fast"]
+
+    config_file_path = _find_config_file()
+    config = _load_config_file_or_default(config_file_path)
+    _log_config_entries(config)
+    _update_config_with_missing_entries_inplace(config)
+
     files: List[str] = find_gd_files_from_paths(
-        arguments["<path>"], excluded_directories=set(".git")
+        arguments["<path>"], excluded_directories=set(config["excluded_directories"])
     )
 
     if files == ["-"]:
@@ -76,6 +94,56 @@ def main():
         )
     else:
         _format_files(files, line_length, spaces_for_indent, safety_checks)
+
+
+def _dump_default_config() -> None:
+    # TODO: error handling
+    assert not os.path.isfile(CONFIG_FILE_NAME)
+    with open(CONFIG_FILE_NAME, "w", encoding="utf-8") as handle:
+        handle.write(yaml.dump(DEFAULT_CONFIG.copy()))
+    sys.exit(0)
+
+
+def _find_config_file() -> Optional[str]:
+    search_dir = pathlib.Path(os.getcwd())
+    config_file_path = None
+    while search_dir != pathlib.Path(os.path.abspath(os.sep)):
+        file_path = os.path.join(search_dir, CONFIG_FILE_NAME)
+        if os.path.isfile(file_path):
+            config_file_path = file_path
+            break
+        file_path = os.path.join(search_dir, ".{}".format(CONFIG_FILE_NAME))
+        if os.path.isfile(file_path):
+            config_file_path = file_path
+            break
+        search_dir = search_dir.parent
+    return config_file_path
+
+
+def _load_config_file_or_default(config_file_path: Optional[str]) -> MappingProxyType:
+    # TODO: error handling
+    if config_file_path is not None:
+        logging.info("Config file found: '%s'", config_file_path)
+        with open(config_file_path, "r", encoding="utf-8") as handle:
+            return yaml.load(handle.read(), Loader=yaml.Loader)
+
+    logging.info("""No 'gdformatrc' nor '.gdformatrc' found. Using default config...""")
+    return DEFAULT_CONFIG
+
+
+def _log_config_entries(config: MappingProxyType) -> None:
+    logging.info("Loaded config:")
+    for entry in config.items():
+        logging.info(entry)
+
+
+def _update_config_with_missing_entries_inplace(config: dict) -> None:
+    for key in DEFAULT_CONFIG:
+        if key not in config:
+            logging.info(
+                "Adding missing entry from defaults: %s", (key, DEFAULT_CONFIG[key])
+            )
+            config[key] = DEFAULT_CONFIG[key]
 
 
 def _format_stdin(
