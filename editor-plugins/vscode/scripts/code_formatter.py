73c9dcaf40e32d55c6b060b7db7460bf55d453b0
nonolai <6914695+nonolai@users.noreply.github.com>
2024-03-18 11:47:31 -0700

    Allow configuring indentation in VSCode extension (#291)

--- editor-plugins/vscode/scripts/code_formatter.py
+++ editor-plugins/vscode/scripts/code_formatter.py
@@ -3,5 +3,11 @@ import io
 from gdtoolkit.formatter import format_code
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
-formatted_code = format_code(sys.argv[1], int(sys.argv[2]))
+
+formatted_code = ""
+if len(sys.argv) == 3:
+    formatted_code = format_code(sys.argv[1], int(sys.argv[2]))
+elif len(sys.argv) == 4:
+    formatted_code = format_code(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
 print(formatted_code)
