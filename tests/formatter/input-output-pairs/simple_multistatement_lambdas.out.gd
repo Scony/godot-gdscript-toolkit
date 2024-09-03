0373e499c6cfe09e082d289646c5ac211a64f45a
Pawel Lampe <pawel.lampe@gmail.com>
2024-09-03 22:33:57 +0200

    Add more 'simple_multistatement_lambdas' tests

--- tests/formatter/input-output-pairs/simple_multistatement_lambdas.out.gd
+++ tests/formatter/input-output-pairs/simple_multistatement_lambdas.out.gd
@@ -8,3 +8,18 @@ func foo():
 	var x3 = func bar():
 		pass
 		123
+
+
+func baz():
+	var x1 = func(x):
+		var y = x + 1
+		if y:
+			return 1
+		return 2
+	var x2 = func():
+		if true:
+			var x = 1
+			if x > 0:
+				return 5
+			return 6
+		return 7
