d5a7204a6a37e4597611676a09461c4024c20bfe
Pawel Lampe <pawel.lampe@gmail.com>
2024-10-20 20:35:41 +0200

    Advance version for 'master'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="gdtoolkit",
-    version="4.3.2",
+    version="4.3.3",
     description="Independent set of tools for working with GDScript - parser, linter and formatter",
     keywords=["GODOT", "GDSCRIPT", "PARSER", "LINTER", "FORMATTER"],
     url="https://github.com/Scony/godot-gdscript-toolkit",
