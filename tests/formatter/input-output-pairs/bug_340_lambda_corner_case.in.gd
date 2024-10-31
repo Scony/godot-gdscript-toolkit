16a4b38db8c2bcae895487c3098597156f3a2f0c
Pawel Lampe <pawel.lampe@gmail.com>
2024-10-31 12:12:54 +0100

    WIP

---
+++ tests/formatter/input-output-pairs/bug_340_lambda_corner_case.in.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+
+	var reports: = []
+	var test_report: int = reports.filter(func (value: int) -> bool:
+		return value == 42
+		).back()
