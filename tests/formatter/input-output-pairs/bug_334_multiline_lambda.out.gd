4b26235f2fd2cf8e0b348c9342f2cf94341f5bf2
shap-po <50052124+shap-po@users.noreply.github.com>
2025-08-05 21:35:02 +0300

    fix: Formatter adding unnecessary extra parenthesis around expressions inside function calls and arrays
    closes #305

--- tests/formatter/input-output-pairs/bug_334_multiline_lambda.out.gd
+++ tests/formatter/input-output-pairs/bug_334_multiline_lambda.out.gd
@@ -6,12 +6,10 @@ class WeaponSystemBullet:
 func foo():
 	var bullet_scene
 	assert(
-		(
-			(func() -> bool:
-				var test_instance: Node = bullet_scene.instantiate()
-				var is_needed_class: bool = test_instance is WeaponSystemBullet
-				test_instance.free()
-				return is_needed_class)
-			. call()
-		)
+		(func() -> bool:
+			var test_instance: Node = bullet_scene.instantiate()
+			var is_needed_class: bool = test_instance is WeaponSystemBullet
+			test_instance.free()
+			return is_needed_class)
+		. call()
 	)
