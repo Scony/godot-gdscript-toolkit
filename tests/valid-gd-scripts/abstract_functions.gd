9c1f8e4580b6cb75616de9b04a4034a5e9919d9a
Pawel Lampe <pawel.lampe@gmail.com>
2025-10-07 20:21:05 +0200

    Add tests for abstract functions, #398

---
+++ tests/valid-gd-scripts/abstract_functions.gd
@@ -0,0 +1,17 @@
+@abstract
+extends Node
+
+@abstract func foo()
+
+@abstract func bar() -> void
+
+@abstract func baz(a) -> void
+
+@abstract func bat(a: int) -> void
+
+@abstract func bak(a: int = 6) -> void
+
+@abstract func ban(a := 6) -> void
+
+@abstract
+func bal() -> void
